38ca89ecd6541d2579507655ad6e5fefb9c8ca7c
calle482 <calle482>
2025-03-22 18:28:00 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+
+  description = "My first flake";
+  
+  inputs = {
+      nixpkgs.url= "github:NixOS/nixpkgs/nixos-24.11";
+};
+
+  outputs = { self, nixpkgs, ... }: 
+    let
+      lib = nixpkgs.lib;
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+    };
+  };
+};
+
+}
