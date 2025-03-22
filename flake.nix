d24c49abfc51d4df889d683668bbf525267e506b
calle482 <calle.sjolund@gmail.com>
2025-03-23 00:51:14 +0100

    minecraft?!?!?!

--- flake.nix
+++ flake.nix
@@ -1,19 +1,24 @@
 {
 
   description = "My first flake";
-  
+
   inputs = {
       nixpkgs.url= "github:NixOS/nixpkgs/nixos-24.11";
+      nix-minecraft.url = “github:Infinidoge/nix-minecraft”;
 };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
     in {
     nixosConfigurations = {
       nixos = lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules =
+        [ ./configuration.nix
+          ./modules/media.nix
+          ./modules/minecraft.nix
+        ];
     };
   };
 };
---
+++ modules/minecraft.nix
@@ -0,0 +1,37 @@
+# configuration.nix. Example of all options that are explained below.
+
+{ pkgs, lib, inputs, … }:
+
+{
+  imports = [ inputs.nix-minecraft.nixosModules.minecraft-servers ];
+  nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    servers = {
+      survival = {
+        enable = true;
+        package = pkgs.fabricServers.fabric-1_21_4;
+        dataDir = “/apps/minecraft/survival”;
+        serverProperties = {
+          gamemode = "survival";
+          difficulty = "normal";
+          simulation-distance = "32"
+        };
+        whitelist = {/* */};
+          symlinks = {
+            mods = pkgs.linkFarmFromDrvs "mods" (
+              builtins.attrValues {
+                Fabric-API = pkgs.fetchurl {
+                  url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/bQZpGIz0/fabric-api-0.119.2%2B1.21.4.jar";
+                  sha512 = "bb8de90d5d1165ecc17a620ec24ce6946f578e1d834ddc49f85c2816a0c3ba954ec37e64f625a2f496d35ac1db85b495f978a402a62bbfcc561795de3098b5c9";
+                };
+              }
+            );
+          };
+        };
+      };
+    };
+};
