cee2917ac3623e14a8ccfb9cc474160d92374b02
calle482 <calle.sjolund@gmail.com>
2025-03-22 19:12:11 +0100

    teeest

--- media.nix
+++ media.nix
@@ -9,7 +9,7 @@
   users.users.media = {
     isNormalUser = true;
     description = "User for media server related applications";
-    extraGroups = [ "media"];
+    extraGroups = [ "media", "jellyfin"];
     packages = with pkgs; [];
   };
 
@@ -25,6 +25,8 @@
   services.jellyfin = {
     enable = true;
     openFirewall = true;
+    user: "media"
+    group: "media"
   };
 
 }
