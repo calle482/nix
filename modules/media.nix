27c5fabff79cacfd8872a34fd6503f72754bb910
calle482 <calle.sjolund@gmail.com>
2025-03-23 04:17:11 +0100

    REEEEEEE

--- modules/media.nix
+++ modules/media.nix
@@ -29,19 +29,6 @@
     pkgs.wireguard-tools
   ];
 
-
-
-  networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-
-  services.resolved = {
-    enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    dnsovertls = "true";
-  };
-
-
   systemd.services."netns@" = {
     description = "%I network namespace";
     before = [ "network.target" ];
