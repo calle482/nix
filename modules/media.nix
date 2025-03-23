607d81dea734ddba8ac5c03702f465b6cffb26c4
calle482 <calle.sjolund@gmail.com>
2025-03-23 03:05:23 +0100

    big vpn test

--- modules/media.nix
+++ modules/media.nix
@@ -10,6 +10,20 @@
 
 
 
+
+  # creating VPN network namespace
+  systemd.services."netns@" = {
+   description = "%I network namespace";
+   before = [ "network.target" ];
+   serviceConfig = {
+     Type = "oneshot";
+     RemainAfterExit = true;
+     ExecStart = "${pkgs.iproute2}/bin/ip netns add %I";
+     ExecStop = "${pkgs.iproute2}/bin/ip netns del %I";
+   };
+  };
+
+
   # Create media group
   users.groups.media = {};
 
@@ -29,6 +43,68 @@
     pkgs.jellyfin-ffmpeg
     pkgs.qbittorrent-nox
   ];
+
+
+
+  # setting up wireguard interface within network namespace
+  systemd.services.wg = {
+   description = "wg network interface";
+   bindsTo = [ "netns@wg.service" ];
+   requires = [ "network-online.target" ];
+   after = [ "netns@wg.service" ];
+   serviceConfig = {
+     Type = "oneshot";
+     RemainAfterExit = true;
+     ExecStart = with pkgs; writers.writeBash "wg-up" ''
+       see -e
+       ${iproute2}/bin/ip link add wg0 type wireguard
+       ${iproute2}/bin/ip link set wg0 netns wg
+       ${iproute2}/bin/ip -n wg address add 10.139.184.160/32 dev wg0
+       # ${iproute2}/bin/ip -n wg -6 address add fd7d:76ee:e68f:a993:e343:5067:2ee2:1a23/128 dev wg0
+       ${iproute2}/bin/ip netns exec wg \
+         ${wireguard-tools}/bin/wg setconf wg0 /root/myVPNprovider.conf
+       ${iproute2}/bin/ip -n wg link set wg0 up
+       # need to set lo up as network namespace is started with lo down
+       ${iproute2}/bin/ip -n wg link set lo up
+       ${iproute2}/bin/ip -n wg route add default dev wg0
+       # ${iproute}/bin/ip -n wg -6 route add default dev wg0
+     '';
+     ExecStop = with pkgs; writers.writeBash "wg-down" ''
+       ${iproute2}/bin/ip -n wg route del default dev wg0
+       # ${iproute2}/bin/ip -n wg -6 route del default dev wg0
+       ${iproute2}/bin/ip -n wg link del wg0
+     '';
+   };
+  };
+
+  # binding qbittorrent to VPN network namespace
+  systemd.services.qbittorrent.bindsTo = [ "netns@wg.service" ];
+  systemd.services.qbittorrent.requires = [ "network-online.target" "wg.service" ];
+  systemd.services.qbittorrent.serviceConfig.NetworkNamespacePath = [ "/var/run/netns/wg" ];
+
+  # allowing qbittorrent web access in network namespace, a socket is necesarry
+  systemd.sockets."proxy-to-vpn-network-namespace" = {
+   enable = true;
+   description = "Socket for Proxy to Qbittorrent Daemon";
+   listenStreams = [ "8080" ];
+   wantedBy = [ "sockets.target" ];
+  };
+
+
+  # creating proxy service on socket, which forwards the same port from the root namespace to the isolated namespace
+  systemd.services."proxy-to-vpn-network-namespace" = {
+   enable = true;
+   description = "Proxy to Qbittorrent Daemon in Network Namespace";
+   requires = [ "qbittorrent.service" "proxy-to-vpn-network-namespac.socket" ];
+   after = [ "qbittorrent.service" ".proxy-to-vpn-network-namespacsocket" ];
+   unitConfig = { JoinsNamespaceOf = "qbittorrent.service"; };
+   serviceConfig = {
+     User = "media";
+     Group = "media";
+     ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:8080";
+     PrivateNetwork = "yes";
+   };
+  };
 
   # Jellyfin service
   services.jellyfin = {
