56901741294cc9489d5a787cadd40d089a98e914
calle482 <calle.sjolund@gmail.com>
2025-03-24 19:01:40 +0100

    disable caddy för nu

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -8,14 +8,14 @@
 
 
 
-services.caddy = {
-  enable = true;
-  package = pkgs-unstable.caddy.withPlugins {
-    plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-    hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
-  };
-  configFile = ./caddyfile;
-};
+# services.caddy = {
+#   enable = true;
+#   package = pkgs-unstable.caddy.withPlugins {
+#     plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+#     hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
+#   };
+#   configFile = ./caddyfile;
+# };
 
 networking.firewall.allowedTCPPorts = [ 80 443 ];
 
