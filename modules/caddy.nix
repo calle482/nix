f98e3ecf64ace41e03b1e40d684d05aa4ce15bc3
calle482 <calle.sjolund@gmail.com>
2025-03-24 18:53:58 +0100

    hmmm

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -11,8 +11,8 @@
 services.caddy = {
   enable = true;
   package = pkgs-unstable.caddy.withPlugins {
-    plugins = [ "github.com/caddy-dns/cloudflare@v0.1.3" ];
-    hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+    plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+    hash = "sha256-AoW35l7QkXunjBzZ43IlyU3UkVXw2D4eyc1jx8xpT0U=";
   };
 };
 
