6c228a177c5bab3008897f0598406067151557e6
calle482 <calle.sjolund@gmail.com>
2025-03-24 18:50:53 +0100

    versiooon

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -11,7 +11,7 @@
 services.caddy = {
   enable = true;
   package = pkgs-unstable.caddy.withPlugins {
-    plugins = [ "https://github.com/caddy-dns/cloudflare" ];
+    plugins = [ "https://github.com/caddy-dns/cloudflare@v0.1.3" ];
     hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
   };
 };
