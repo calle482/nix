3733ccbed2000447ecfbc026c7f986aa48a4eaf3
calle482 <calle.sjolund@gmail.com>
2025-03-26 19:36:23 +0100

    persist ssh host keys

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -94,6 +94,10 @@
     files = [
       "/etc/machine-id"
       "/root/myVPNprovider.conf"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
       { file = "/etc/nix/id_rsa"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
   };
