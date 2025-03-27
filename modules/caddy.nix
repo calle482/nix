cba2f0356ea8eecac1e222a0b9751f0f0b57b952
calle482 <calle.sjolund@gmail.com>
2025-03-27 23:49:41 +0100

    teeest

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -20,7 +20,9 @@ sops.secrets."cloudflare/api_key" = {
 
 services.caddy = {
   enable = true;
-  environment = "test123";
+  environment = {
+    CF_API_TOKEN = "test123";
+  };
   package = pkgs-unstable.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
     hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
