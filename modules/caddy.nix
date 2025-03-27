23a14c9851f150a7e158b83e2fbb9d84064d6478
calle482 <calle.sjolund@gmail.com>
2025-03-27 23:47:53 +0100

    teeest

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -20,7 +20,7 @@ sops.secrets."cloudflare/api_key" = {
 
 services.caddy = {
   enable = true;
-  EnvironmentFile = "${{config.sops."cloudflare_api_token".path }}";
+  EnvironmentFile = /run/secrets/cloudflare/api_key;
   package = pkgs-unstable.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
     hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
