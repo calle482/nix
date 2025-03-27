bc1cdce3b1cbb68e591e6c30ec3855c61f165bfd
calle482 <calle.sjolund@gmail.com>
2025-03-27 23:03:37 +0100

    lite test

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -2,20 +2,25 @@
 
 {
 
-  #environment.systemPackages = with pkgs-unstable; [
-  #  caddy
-  #];
+# Secrets
+sops.secrets."cloudflare/api_key" = {
+
+};
+
+  environment.systemPackages = with pkgs-unstable; [
+    caddy
+  ];
 
 
 
-# services.caddy = {
-#   enable = true;
-#   package = pkgs-unstable.caddy.withPlugins {
-#     plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-#     hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
-#   };
-#   configFile = ./caddyfile;
-# };
+services.caddy = {
+  enable = true;
+  package = pkgs-unstable.caddy.withPlugins {
+    plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+    hash = "sha256-W09nFfBKd+9QEuzV3RYLeNy2CTry1Tz3Vg1U2JPNPPc=";
+  };
+  configFile = ./caddyfile;
+};
 
 networking.firewall.allowedTCPPorts = [ 80 443 ];
 
