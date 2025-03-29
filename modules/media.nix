d52efcbdb9f3dc0d70d1ad4bb7cdf4f95acf773e
calle482 <calle.sjolund@gmail.com>
2025-03-29 17:35:25 +0100

    teeest

--- modules/media.nix
+++ modules/media.nix
@@ -260,11 +260,11 @@
     ProtectKernelModules = true;
     ProtectControlGroups = true;
     PrivateDevices = true;
-    #RestrictSUIDSGID = true;
-    #ProtectClock = true;
-    #PrivateUsers = true;
-    #ProtectHome = true;
-    #SystemCallFilter = [ "~@clock" "~@cpu-emulation" "~@debug" "~@module" "~@mount" "~@obsolete" "~@privileged" "~@raw-io" "~@reboot" "~@resources" "~@swap"];
+    RestrictSUIDSGID = true;
+    ProtectClock = true;
+    PrivateUsers = true;
+    ProtectHome = true;
+    SystemCallFilter = [ "~@clock" "~@cpu-emulation" "~@debug" "~@module" "~@mount" "~@obsolete" "~@privileged" "~@raw-io" "~@reboot" "~@resources" "~@swap"];
     #ProtectKernelLogs = true;
     #RestrictRealtime = true;
     #SystemCallArchitectures = "native";
