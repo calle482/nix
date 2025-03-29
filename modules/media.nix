d9971bc76f9482a337fc24f42b747d364f6fa116
calle482 <calle.sjolund@gmail.com>
2025-03-29 18:13:49 +0100

    hmm

--- modules/media.nix
+++ modules/media.nix
@@ -264,8 +264,8 @@
     #ProtectClock = true;
     PrivateUsers = true;
     ProtectHome = true;
-    SystemCallFilter = [ "~@cpu-emulation" "~@debug" "~@module" "~@mount" "~@obsolete" "~@privileged" "~@raw-io" "~@reboot" "~@resources" "~@swap"];
-    ReadWritePaths = ["/apps/radarr"];
+    #SystemCallFilter = [ "~@clock" "~@cpu-emulation" "~@debug" "~@module" "~@mount" "~@obsolete" "~@privileged" "~@raw-io" "~@reboot" "~@resources" "~@swap"];
+    ReadWritePaths = ["/apps/radarr"]
    # ProtectKernelLogs = true;
    # RestrictRealtime = true;
    # SystemCallArchitectures = "native";
