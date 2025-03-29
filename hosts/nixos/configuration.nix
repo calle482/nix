5b7145b37a5588e859a5e14e40768d37ea444c01
calle482 <calle.sjolund@gmail.com>
2025-03-29 02:19:13 +0100

    kanske?

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -89,7 +89,7 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      #"/run/secrets"
+      "/run/secrets.d"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
     ];
     files = [
