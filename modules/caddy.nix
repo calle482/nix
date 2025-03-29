687189121d35e0dbf40380f72db2d26328e2e55e
calle482 <calle.sjolund@gmail.com>
2025-03-29 23:54:00 +0100

    eksdee

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -51,7 +51,6 @@ systemd.services.caddy = {
     ProtectProc = true;
     RemoveIPC = true;
     MemoryDenyWriteExecute = true;
-    PrivateUsers = true;
     ReadOnlyPaths = config.sops.secrets."cloudflare/api_key".path;
     EnvironmentFile = config.sops.secrets."cloudflare/api_key".path;
   };
