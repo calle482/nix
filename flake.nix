0ed07653e6e2d1bfa1f21d7ce102bac4f5759e1c
calle482 <calle.sjolund@gmail.com>
2025-04-17 19:14:17 +0200

    det är ju inte lätt att vara dum i huvudet

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       nix-minecraft.url = "github:Infinidoge/nix-minecraft";
       impermanence.url = "github:nix-community/impermanence";
       sops-nix.url = "github:Mic92/sops-nix";
-      home-manager.url = "github:nix-community/home-manager";
+      home-manager.url = "github:nix-community/home-manager/release-24.11";
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
 };
 
