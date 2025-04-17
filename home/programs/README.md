44f6cbbacbd2615d68d0dc226bdcf03a0c089596
calle482 <calle.sjolund@gmail.com>
2025-04-17 23:19:16 +0200

    sonarr recyclarr conf

---
+++ home/programs/README.md
@@ -0,0 +1,7 @@
+# Recyclarr
+Secrets file needs to be created manually at .config/recyclarr/secrets.yml
+
+```
+radarr_api_key: XXXXX
+sonarr_api_key: XXXXX
+```
--- home/programs/recyclarr.nix
+++ home/programs/recyclarr.nix
@@ -5,14 +5,22 @@
     pkgs.recyclarr
   ];
 
-  sops.secrets."radarr/api_key" = { };
-  sops.secrets."sonarr/api_key" = { };
-
-
-  home.file.".config/recyclarr/secrets.yml".text = ''
-    radarr_api_key: "${config.sops.secrets."radarr/api_key".path}"
-    sonarr_api_key: "${config.sops.secrets."sonarr/api_key".path}"
-  '';
+#   sops.secrets."radarr/api_key" = { };
+#   sops.secrets."sonarr/api_key" = { };
+#
+#
+#   sops.templates."recyclarr-secrets" = {
+#     content = ''
+#       radarr_api_key: ${config.sops.placeholder."radarr/api_key"}
+#       sonarr_api_key: ${config.sops.placeholder."sonarr/api_key"}
+#     '';
+#   };
+#
+#
+#  home.file.".config/recyclarr/secrets.yml".text = ''
+#    radarr_api_key: "${config.sops.secrets."radarr/api_key".path}"
+#    sonarr_api_key: "${config.sops.secrets."sonarr/api_key".path}"
+#  '';
 
   xdg.configFile."recyclarr/recyclarr.yml".source = ./config/recyclarr.yml;
 }
