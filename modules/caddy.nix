c1c3ace7adadb02bf2e63f1eb7ac1ed22b9fa660
nix <calle@nix.local>
2025-05-24 01:13:57 +0200

    update hash

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -22,7 +22,7 @@ services.caddy = {
   enable = true;
   package = pkgs-unstable.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" "github.com/caddyserver/transform-encoder@v0.0.0-20231219065943-58ebafa572d5" ];
-    hash = ["sha256-seEPxuaR71Nq7EM2hcQFbzVj8TeZM7RDXvn6hzuG0+8="];
+    hash = ["sha256-pdC6ZCgwjc7+dqxwQ82bUyTPJIHcL7B0cVq9jyDrwhU="];
   };
   configFile = ./caddyfile;
 };
